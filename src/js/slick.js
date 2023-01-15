5a27402886eb86e562a8c345ce78c18cfbcda638
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-01-15 22:39:00 +0200

    fix slick

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,9 +1,20 @@
-$('.one-time').slick({
+// $('.one-time').slick({
+//   dots: false,
+//   arrows: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//     adaptiveHeight: true,
+//     autoplay: true,
+// });
+
+$('.variable-width').slick({
   dots: false,
-  arrows: true,
   infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-    adaptiveHeight: true, 
-    autoplay: true,
+  speed: 1000,
+  slidesToShow: 3,
+  autoplay: true,
+  // centerMode: true,
+  variableWidth: true,
+  autoplaySpeed: 500,
 });
