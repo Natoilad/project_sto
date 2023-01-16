c59f7f44d18a9ab9c6cb17b86ea22f8d65433bbf
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-01-16 02:06:42 +0200

    update valid

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,16 +1,3 @@
-// $('.one-time').slick({
-//   dots: false,
-//   arrows: true,
-//   infinite: true,
-
-//   speed: 1000,
-//   slidesToShow: 3,
-//   autoplay: true,
-//   // centerMode: true,
-//   variableWidth: true,
-//   autoplaySpeed: 500,
-// });
-
 $('.multiple-items').slick({
   dots: false,
   infinite: true,
